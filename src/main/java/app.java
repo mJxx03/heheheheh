d35fbf89c6d348ccc64579a3dab72383ea070b5e
MJ <MJ@DESKTOP-F1KUSDN>
2023-10-31 18:13:45 +0530

    first

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,13 +1,12 @@
 package myproject; 
-import java.util.ResourceBundle; 
-public class app{
-	public int userlogin(String in_user,String in_pwd) { 
-		ResourceBundle rb=ResourceBundle.getBundle("config"); 
-		String uname=rb.getString("in_user");
-		String pwd=rb.getString("in_pwd"); 
-		if(in_user.equals(uname)&& in_pwd.equals(pwd)) 
-			return 1; 
-		else 
-			return 0;
-		} 
+import java.util.ResourceBundle;
+public class app{ public int userlogin(String in_name,String in_id) {
+	ResourceBundle rb=ResourceBundle.getBundle("config"); 
+	String uname=rb.getString("in_name");
+	String id=rb.getString("in_id");
+	if(in_name.equals(uname)&& in_id.equals(id)) 
+		return 1; 
+	else 
+		return 0;
 	}
+}
--- src/test/java/myproject/Apptest.java
+++ src/test/java/myproject/Apptest.java
@@ -4,10 +4,10 @@ import org.testng.annotations.Test;
 public class apptest { 
 	public void testlogin1() {
 		app myapp=new app(); 
-		Assert.assertEquals(0,myapp.userlogin("abc","abc123")); 
+		Assert.assertEquals(0,myapp.userlogin("abc","9054")); 
 		}
 	public void testlogin2() {
 		app myapp=new app();
-		Assert.assertEquals(0,myapp.userlogin("Vit","vit123")); 
+		Assert.assertEquals(0,myapp.userlogin("ram","3025")); 
 		}
 	}
